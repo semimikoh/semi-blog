abb1de75bb7b95b0bd30d9f2bffc0bd5ab2ba1b1
semimikoh <ejffjeosms@gmail.com>
2026-03-23 11:23:28 +0900

    feat: Kubernetes 운영 포스트 추가 및 태그 순서 고정

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -6,7 +6,20 @@ export default function PostsPage() {
     (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime(),
   );
 
-  const allTags = [...new Set(posts.flatMap((post) => post.tags))];
+  const TAG_ORDER = [
+    'React',
+    'JavaScript',
+    '에디터',
+    '브라우저',
+    '알고리즘',
+    '클라우드',
+    'Docker',
+    'Kubernetes',
+    '인프라',
+  ];
+  const allTags = TAG_ORDER.filter((tag) =>
+    posts.some((post) => post.tags.includes(tag)),
+  );
 
   return (
     <div className="flex flex-col gap-5 pt-3 pb-16">
